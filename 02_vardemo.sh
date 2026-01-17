b38daca082842f5fa12f861a54779689c7e6a0cc
pradeepgitbit <pksr884488@gmail.com>
2026-01-17 15:10:40 +0530

    bash fundas

---
+++ 02_vardemo.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+a=10
+name="Pardeep"
+age=24
+
+echo "My name is $name and age is $age"
+
+name="kumar"
+
+echo "My name is $name"
+
+# Var to store the hostname
+
+HOSTNAME=$(hostname)
+
+echo "Hostname is $HOSTNAME"
---
+++ 04_constant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Constant variable
+
+readonly COLLEGE="DCRUST"
+
+echo "My college name is $COLLEGE"
+
+COLLEGE="test"
