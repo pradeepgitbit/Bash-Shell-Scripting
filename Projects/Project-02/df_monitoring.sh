8d32fdad691b07b81c1b0173fc0aec50d5f5d665
pradeepgitbit <pksr884488@gmail.com>
2026-01-21 23:57:05 +0530

    send-email

--- Projects/Project-02/df_monitoring.sh
+++ Projects/Project-02/df_monitoring.sh
@@ -2,9 +2,11 @@
 
 FS=$(df -h | grep "/mnt/c" | awk '{print $5}' | tr -d %)
 
+TO="pksr884488@gmail.com"
+
 if [[ $FS -gt 50 ]]
 then
-    echo "Warning, disk space is low"
+    echo "Warning, disk space is low - $FS %" | mail -s "Disk Space Alert!" $TO
 else
     echo "All good"
 fi
