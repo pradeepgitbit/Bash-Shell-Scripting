709b904e61c75a84fae14477a59ef28a55371824
pradeepgitbit <pksr884488@gmail.com>
2026-01-22 19:34:29 +0530

    ALL conditions

--- Projects/Project-03/user_create.sh
+++ Projects/Project-03/user_create.sh
@@ -29,13 +29,34 @@ echo $PASSWORD
 
 
 # Create the User
-
+useradd -c $COMMENT -m $USER_NAME
 
 
 # Check if user is successfully created or not
-
+if [[ $? -ne 0 ]]
+then
+    echo "The Account could not be created"
+    exit 1
+fi
 
 # Set the password for the uer
-
+passwd $PASSWORD $USER_NAME
 
 # Check if password is succesfully set or not
+if [[ $? -ne 0 ]]
+then
+    echo "Password could not be set"
+    exit 1
+fi
+
+# Force password change on first login.
+passwd -e $USER_NAME
+
+# Display the username, password, and the host where the user was created.
+
+echo
+echo "Username: $USER_NAME"
+echo
+echo "Password: $PASSWORD"
+echo
+echo $(hostname)
