1d1ca4101aec820e6a7feacc3649c2840a77c187
pradeepgitbit <pksr884488@gmail.com>
2026-01-22 18:56:42 +0530

    added steps

---
+++ Projects/Project-03/user_create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Script should be executed with sudo/root access.
+
+
+# User should provide atleast one argument as username else guide him
+
+
+# Store 1st argument as user name
+
+
+# In case of more than one argument, store is as account arguments
+
+
+# Create a password
+
+
+# Create the User
