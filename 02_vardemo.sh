bedab1136615784c1c4a603aa2f71ecf75934d8a
pradeepgitbit <pksr884488@gmail.com>
2026-03-19 22:14:13 +0530

    variabledemo

--- 02_vardemo.sh
+++ 02_vardemo.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+
+#How to assign Variables
+
 a=10
 name="Pardeep"
 age=24
+college="DCRUST"
 
-echo "My name is $name and age is $age"
+echo "My name is $name and age is $age and my college is $college"
+
+
+# Assign different value to the same variable
 
 name="kumar"
 
