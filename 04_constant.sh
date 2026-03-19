34f538a17e7a0dba721d68fabcb34dbefa813adb
pradeepgitbit <pksr884488@gmail.com>
2026-03-19 22:16:48 +0530

    constant-variable

--- 04_constant.sh
+++ 04_constant.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-#Constant variable
+#Constant variable (variable will be same even if changed afterwards)
 
 readonly COLLEGE="DCRUST"
 
 echo "My college name is $COLLEGE"
 
+
+# try to change the variable value
+
 COLLEGE="test"
+
+echo "My college name is $COLLEGE"
