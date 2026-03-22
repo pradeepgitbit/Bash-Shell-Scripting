c5910c7307fafd42a863dbd98cc21feaba540a04
pradeepgitbit <pksr884488@gmail.com>
2026-03-22 23:23:19 +0530

    div-added

--- 09_arith_ops.sh
+++ 09_arith_ops.sh
@@ -7,9 +7,12 @@ y=2
 let mul=$x*$y
 echo "$mul"
 
+let div=$x/$y
+echo "$div"
+
 let sum=$x+$y
 echo "$sum"
 
-#using (())
+#using double paranthesis (()) for operations instead of let.
 
 echo "subtraction is $(($x-$y))"
